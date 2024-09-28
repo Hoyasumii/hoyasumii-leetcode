da9dd978f74b8f474dbfc65d37fb49052a956914
Hoyasumii <alanreisanjo@gmail.com>
2024-09-28 12:13:15 -0300

    1. TwoSum - O(n)

--- TwoSum.ts
+++ TwoSum.ts
@@ -1,13 +1,13 @@
-export function twoSum(nums: number[], target: number): number[] | undefined {
-  for (let firstIndex: number = 0; firstIndex < nums.length; firstIndex++) {
-    for (
-      let lastIndex: number = nums.length - 1;
-      lastIndex > firstIndex;
-      lastIndex--
-    ) {
-      if (nums[firstIndex] + nums[lastIndex] === target) {
-        return [firstIndex, lastIndex];
-      }
+function twoSum(nums: number[], target: number): number[] | undefined {
+  let mappedNumbers: Map<number, number> = new Map();
+
+  for (let index: number = 0; index < nums.length; index++) {
+    const value = target - nums[index];
+
+    if (mappedNumbers.has(value)) {
+      return [mappedNumbers.get(value) || 0, index];
+    } else {
+      mappedNumbers.set(nums[index], index);
     }
   }
 }
